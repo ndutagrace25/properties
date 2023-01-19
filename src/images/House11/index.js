118a724193befb1155cd0fa527f22e0c110ffb36
Grace Nduta <ndutagrace25@gmail.com>
2023-01-19 15:34:28 +0300

    pushing all changes

---
+++ src/images/House11/index.js
@@ -0,0 +1,23 @@
+import imageonerN from "./1.jpg";
+import imagetworbN from "./2.jpg";
+import imagethreerN from "./3.jpg";
+import imagefourrN from "./4.jpg";
+import imagefiverN from "./5.jpg";
+import imagesixrN from "./6.jpg";
+import imagesevenrN from "./7.jpg";
+import imageeightrN from "./8.jpg";
+import imageninerN from "./9.jpg";
+import imagetenrN from "./10.jpg";
+
+export {
+  imageonerN,
+  imagetworbN,
+  imagethreerN,
+  imagefourrN,
+  imagefiverN,
+  imagesixrN,
+  imagesevenrN,
+  imageeightrN,
+  imageninerN,
+  imagetenrN,
+};
